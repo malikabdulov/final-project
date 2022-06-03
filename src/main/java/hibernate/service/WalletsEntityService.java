81daa8f63413c72ed5a51725515137257d23d68f
malik.abdulov <malik.abdulov@gmail.com>
2022-06-03 20:54:27 +0600

    stage 3 DONE

--- src/main/java/hibernate/service/WalletsEntityService.java
+++ src/main/java/hibernate/service/WalletsEntityService.java
@@ -32,6 +32,16 @@ public class WalletsEntityService {
         walletsEntityDao.closeCurrentSessionWithTransaction();
     }
 
+    public void update(WalletsEntity walletsEntity1, OperationsEntity operationsEntity1, WalletsEntity walletsEntity2, OperationsEntity operationsEntity2) {
+        walletsEntityDao.openCurrentSessionWithTransaction();
+        operationsEntityDao.setCurrentSession(walletsEntityDao.getCurrentSession());
+        operationsEntityDao.persist(operationsEntity1);
+        walletsEntityDao.update(walletsEntity1);
+        operationsEntityDao.persist(operationsEntity2);
+        walletsEntityDao.update(walletsEntity2);
+        walletsEntityDao.closeCurrentSessionWithTransaction();
+    }
+
     public WalletsEntity findById(String id) {
         walletsEntityDao.openCurrentSession();
         WalletsEntity walletsEntity = walletsEntityDao.findById(id);
@@ -57,5 +67,22 @@ public class WalletsEntityService {
         operationsEntity.setWallet_id(walletsEntity.getId());
         walletsEntity.decreaseBalance(delta);
         this.update(walletsEntity, operationsEntity);
+    }
+
+    public void transferMoney(WalletsEntity sender, WalletsEntity recipient, Long delta) {
+        OperationsEntity operationsEntitySender = new OperationsEntity();
+        operationsEntitySender.setAmount(delta);
+        operationsEntitySender.setType_id(3);
+        operationsEntitySender.setCreated_at(new Timestamp(new Date().getTime()));
+        operationsEntitySender.setWallet_id(sender.getId());
+        sender.decreaseBalance(delta);
+
+        OperationsEntity operationsEntityReceiver = new OperationsEntity();
+        operationsEntityReceiver.setAmount(delta);
+        operationsEntityReceiver.setType_id(4);
+        operationsEntityReceiver.setCreated_at(new Timestamp(new Date().getTime()));
+        operationsEntityReceiver.setWallet_id(recipient.getId());
+        recipient.increaseBalance(delta);
+        this.update(sender, operationsEntitySender, recipient, operationsEntityReceiver);
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Balance balance = new Balance();
-        System.out.println("getBalance " + balance.getBalance("1").toString());
-        System.out.println("putMoney " + balance.putMoney("1", 200L).toString());
-        System.out.println("takeMoney " + balance.takeMoney("1", 200L).toString());
-        System.out.println("getOperationsList " + balance.getOperations("1", "2022-06-01", "2022-06-02").toString());
+//        System.out.println("getBalance " + balance.getBalance("1"));
+//        System.out.println("putMoney " + balance.putMoney("1", 200L));
+//        System.out.println("takeMoney " + balance.takeMoney("1", 200L));
+//        System.out.println("getOperationsList " + balance.getOperations("1", "2022-06-01", "2022-06-02"));
+//        System.out.println("transferMoney " + balance.transferMoney("2", "1", 250L));
+        System.out.println("transferMoney " + balance.transferMoney("2", "2", 250L));
 
     }
 }
